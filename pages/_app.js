b10f17062378c804ae2aeaf257d474bd1926beab
abdulrahmansaja2002 <abdul.rahman.saja2002@gmail.com>
2023-12-05 14:00:13 +0700

    add head tag on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head >
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="A search engine for programmers." />
+        <meta name="keywords" content="search engine, programmers, programming, coding, computer science, computer, science, physics, math, gaming, articles, blogs, tutorials, videos, books, courses, programming language, programming languages, programming language, programming languages, program " />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <meta name="description" content="A search engine for programmers."></meta>
       <body>
         <Main />
         <NextScript />
